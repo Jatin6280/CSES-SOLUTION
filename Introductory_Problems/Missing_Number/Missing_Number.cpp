a484def7423b78797ba9143cc7d9acc9c5cc5ba5
Jatin <hasijajatin436@gmail.com>
2026-08-10 23:48:48 +0530

    Add C++ solution for Missing_Number

---
+++ Introductory_Problems/Missing_Number/Missing_Number.cpp
@@ -0,0 +1,30 @@
+/*
+ * Problem Name: Missing_Number
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-10
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    cin>>n;
+    vector<int>v(n-1);
+    for(int i=1;i<n;i++) {
+        cin>>v[i];
+    }
+    sort(v.begin(), v.end());
+    int ans=v[0];
+    for(int i=0;i<n-1;i++) {
+        if (v[i]+1!=v[i+1]) {
+            ans=v[i]+1;
+            break;
+        }
+    }
+    cout<<ans<<endl;
+}
