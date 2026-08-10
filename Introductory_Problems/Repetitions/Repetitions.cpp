26ede82a541105c4c2769175b6d198dd22a04d55
Jatin <hasijajatin436@gmail.com>
2026-08-11 00:33:03 +0530

    Add C++ solution for Repetitions

---
+++ Introductory_Problems/Repetitions/Repetitions.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Repetitions
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-10
+ */
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:11:19: warning: comparison of integer expressions of different signedness: 'int' and 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   11 |     for (int i=0;i<a.size()-1;i++) {
+      |                  ~^~~~~~~~~~~
