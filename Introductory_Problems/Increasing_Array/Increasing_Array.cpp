8633a894bd65086ffc93db661d9a81e2b2ef9e11
Jatin <hasijajatin436@gmail.com>
2026-08-11 21:29:43 +0530

    Add C++ solution for Increasing_Array

---
+++ Introductory_Problems/Increasing_Array/Increasing_Array.cpp
@@ -0,0 +1,32 @@
+/*
+ * Problem Name: Increasing_Array
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-11
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) {
+        cin>>a[i];
+    }
+    int moves=0;
+    for(int i=1;i<n;i++) {
+        int current=a[i];
+        int prev=a[i-1];
+        if (current<prev) {
+            moves+=prev-current;
+                
+        }
+        cout<<moves<<endl;
+        return 0;
+    }
+}
