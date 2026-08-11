98730c729fe994a4d21d84e6750197e38eb2ab13
Jatin <hasijajatin436@gmail.com>
2026-08-11 20:52:11 +0530

    Add C++ solution for Repetitions

--- Introductory_Problems/Repetitions/Repetitions.cpp
+++ Introductory_Problems/Repetitions/Repetitions.cpp
@@ -6,6 +6,6 @@
  */
 
 input/code.cpp: In function 'int main()':
-input/code.cpp:11:19: warning: comparison of integer expressions of different signedness: 'int' and 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
-   11 |     for (int i=0;i<a.size()-1;i++) {
+input/code.cpp:12:19: warning: comparison of integer expressions of different signedness: 'int' and 'std::__cxx11::basic_string<char>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   12 |     for (int i=0;i<a.size()-1;i++) {
       |                  ~^~~~~~~~~~~
