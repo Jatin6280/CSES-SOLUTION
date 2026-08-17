43002e2ced7f13712a5a367db786895a94150a8a
Jatin <hasijajatin436@gmail.com>
2026-08-17 23:43:16 +0530

    Add C++ solution for Trailing_Zeros

---
+++ Introductory_Problems/Trailing_Zeros/Trailing_Zeros.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem Name: Trailing_Zeros
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-17
+ */
+
+  #include <iostream>
+using namespace std;
+ 
+const long long MOD = 1000000007;
+ 
+int main() {
+    long long n;
+    cin >> n;
+    cout<<n/5;
+}
