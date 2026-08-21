ace3119b3cc0ae29c2121bc706e1c40ca1959f87
Jatin <hasijajatin436@gmail.com>
2026-08-21 14:45:28 +0530

    Add C++ solution for Apple_Division

---
+++ Introductory_Problems/Apple_Division/Apple_Division.cpp
@@ -0,0 +1,11 @@
+/*
+ * Problem Name: Apple_Division
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-08-21
+ */
+
+input/code.cpp: In function 'void divide_apples(std::vector<long long int>&, ll&, int, ll, ll)':
+input/code.cpp:11:13: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<long long int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+   11 |     if (idx == weights.size())
+      |         ~~~~^~~~~~~~~~~~~~~~~
